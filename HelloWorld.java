e2a42ee4976eadbf97df78ed772f5172ee72ab63
luozhipeng-lzp <2657750679@qq.com>
2020-06-20 00:41:53 +0800

    Test

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package demo01;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("何宇");
+		System.out.println("hello fy");//字符
+		System.out.println("77");
+		System.out.println("88");
+		System.out.println("又做了修改");
+	}
+
+}
---
+++ UserTest.java
@@ -0,0 +1,10 @@
+package demo01;
+
+/**
+ * Created by Administrator on 2019/9/17 0017.
+ */
+public class UserTest {
+    public static void main(String[] args){
+        System.out.println("这是一个测试类");
+    }
+}
